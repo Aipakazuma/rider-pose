48d79cc1ce3d7cd780fc23d56b65f2f825c2bca3
aipa <k.tamashiro@churadata.okinawa>
2021-11-22 13:06:12 +0900

    [modify] リファクタ.

---
+++ vue-project/src/components/CameraSelect.vue
@@ -0,0 +1,55 @@
+<template>
+  <p>
+    カメラ:
+    <select v-model="selectedVideo" @change="onChange">
+      <option disabled value="">Please select one</option>
+      <option
+        v-for="(video, key, index) in videos"
+        v-bind:key="index"
+        :value="video.value"
+      >
+        {{ video.text }}
+      </option>
+    </select>
+  </p>
+</template>
+
+<script>
+export default {
+  setup() {},
+  data: () => ({
+    selectedVideo: "",
+    videos: [],
+  }),
+  mounted: async function () {
+    //デバイスへのアクセス
+    await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
+    const deviceInfos = await navigator.mediaDevices.enumerateDevices();
+
+    //カメラの情報を取得
+    deviceInfos
+      .filter((deviceInfo) => deviceInfo.kind === "videoinput")
+      .map((video) =>
+        this.videos.push({
+          text: video.label || `Camera  ${this.videos.length - 1}`,
+          value: video.deviceId,
+        })
+      );
+  },
+  methods: {
+    onChange: function () {
+      if (this.selectedVideo != "") this.connectLocalCamera();
+    },
+    connectLocalCamera: async function () {
+      const constraints = {
+        video: this.selectedVideo
+          ? { deviceId: { exact: this.selectedVideo } }
+          : false,
+      };
+
+      const stream = await navigator.mediaDevices.getUserMedia(constraints);
+      this.$emit("connectLocalCamera", stream);
+    },
+  },
+};
+</script>
--- vue-project/src/components/Video.vue
+++ vue-project/src/components/Video.vue
@@ -1,59 +1,20 @@
 <template>
   <div>
-    <p>
-      カメラ:
-      <select v-model="selectedVideo" @change="onChange">
-        <option disabled value="">Please select one</option>
-        <option
-          v-for="(video, key, index) in videos"
-          v-bind:key="index"
-          :value="video.value"
-        >
-          {{ video.text }}
-        </option>
-      </select>
-    </p>
+    <CameraSelect v-on:connectLocalCamera="connectLocalCamera" />
     <video id="my-video" muted="true" width="500" autoplay playsinline></video>
   </div>
 </template>
 
 <script>
+import CameraSelect from "./CameraSelect.vue";
+
 export default {
   name: "Video",
-  data: () => ({
-    videos: [],
-    selectedVideo: "", //追記
-  }),
-
-  mounted: async function () {
-    //デバイスへのアクセス
-    await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
-    const deviceInfos = await navigator.mediaDevices.enumerateDevices();
-
-    //カメラの情報を取得
-    deviceInfos
-      .filter((deviceInfo) => deviceInfo.kind === "videoinput")
-      .map((video) =>
-        this.videos.push({
-          text: video.label || `Camera  ${this.videos.length - 1}`,
-          value: video.deviceId,
-        })
-      );
+  components: {
+    CameraSelect,
   },
-
   methods: {
-    onChange: function () {
-      if (this.selectedVideo != "") this.connectLocalCamera();
-    },
-
-    connectLocalCamera: async function () {
-      const constraints = {
-        video: this.selectedVideo
-          ? { deviceId: { exact: this.selectedVideo } }
-          : false,
-      };
-
-      const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    connectLocalCamera: async function (stream) {
       document.getElementById("my-video").srcObject = stream;
     },
   },
