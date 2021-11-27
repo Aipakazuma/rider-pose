19f2b4bb65bf58fefc50c443c7ed63a6898a94c7
aipa <k.tamashiro@churadata.okinawa>
2021-11-27 21:40:47 +0900

    [moidfy] とりあえずコミット

--- pytorch-openpose/api.py
+++ pytorch-openpose/api.py
@@ -2,9 +2,13 @@ import cv2
 import copy
 import numpy as np
 import torch
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, BackgroundTasks
 from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import StreamingResponse
 from PIL import Image
+from typing import Dict
+import uuid
+import io
 
 from src import util
 from src.body import Body
@@ -82,9 +86,9 @@ def detect(img, models):
     return canvas, candidate, subset
 
 
-def create_animation_gif(imgs):
+def create_animation_gif(imgs, random_str):
     new_imgs = [Image.fromarray(img) for img in imgs]
-    new_imgs[0].save('output.gif',
+    new_imgs[0].save('output_{}.gif'.format(random_str),
                      save_all=True,
                      append_images=new_imgs[1:],
                      optimize=False,
@@ -92,7 +96,7 @@ def create_animation_gif(imgs):
                      loop=0)
 
 
-def main(body):
+def main(body, random_str):
     try:
         print(f"Torch device: {torch.cuda.get_device_name()}")
     except Exception as e:
@@ -100,7 +104,7 @@ def main(body):
 
     write_post_movie(body)
     results_img, results_point = read_video()
-    create_animation_gif(results_img)
+    create_animation_gif(results_img, random_str)
     print(np.array(results_point))
 
 
@@ -113,8 +117,60 @@ app.add_middleware(
     allow_headers=["*"])
 
 
-@ app.post('/video')
-def post_video(video_data: UploadFile = File(...)):
+class Task():
+    def __init__(self, body, job_id):
+        self.body = body
+        self.job_id = job_id
+        self.random_str = '{}_{}'.format(self.job_id, str(uuid.uuid4()))
+
+    def __call__(self):
+        jobs[self.job_id] = self
+        try:
+            main(self.body, self.random_str)
+            done_jobs[self.job_id] = self.random_str
+        except Exception as e:
+            print(e)
+
+        del jobs[self.job_id]
+
+
+jobs: Dict[int, Task] = {}
+done_jobs: Dict[int, str] = {}
+
+
+@app.get('/video')
+async def job_list():
+    job_ids = [job_id for job_id, _ in jobs.items()]
+    return {"job_ids": job_ids}
+
+
+@app.get('/video/{job_id}')
+async def status_check(job_id: int):
+    return {"status": job_id in jobs}
+
+
+@app.get('/video/output/{job_id}')
+def get_gif(job_id: int):
+    if job_id not in done_jobs:
+        return None
+
+    t = done_jobs[job_id]
+    file_name = './output_{}.gif'.format(t)
+    with open(file_name, 'rb') as f:
+        results = f.read()
+
+    print(file_name)
+    return StreamingResponse(io.BytesIO(results), media_type="image/gif")
+
+
+@app.post('/video/{job_id}')
+def post_video(background_tasks: BackgroundTasks,
+               job_id: int,
+               video_data: UploadFile = File(...)):
     body = video_data.file.read()
-    main(body)
-    return {"message": len(body)}
+    t = Task(body, job_id)
+    background_tasks.add_task(t)
+    return {
+        "upload_file_size": len(body),
+        "job_id": job_id
+    }
--- vue-project/src/components/Video.vue
+++ vue-project/src/components/Video.vue
@@ -1,47 +1,49 @@
 <template>
   <el-container>
-    <el-header>Header</el-header>
+    <el-header><h1>リデデブザガギ、ゴセンゼンギン</h1></el-header>
     <el-container>
-      <el-aside width="200px">
-        <el-row>
-          <el-col>
-            <CameraSelect v-on:connectLocalCamera="connectLocalCamera" />
-          </el-col>
-        </el-row>
-        <el-row>
-          <el-col v-if="isPossibleRecord">
-            <el-button type="primary" v-on:click="startRecording">
-              録画する
-            </el-button>
-          </el-col>
-          <el-col v-else>
-            <el-button type="info" v-on:click="stopRecording"
-              >録画を終了する</el-button
-            >
-          </el-col>
-        </el-row>
-        <el-row>
-          <el-col>
-            <el-button
-              type="primary"
-              v-if="isPossibleDownload"
-              v-on:click="downloadVideo"
-            >
-              ダウンロードする
-            </el-button>
-          </el-col>
-        </el-row>
-        <el-row>
-          <el-col>
-            <el-button
-              type="success"
-              v-if="isPossibleDownload"
-              v-on:click="postVideo"
-            >
-              サーバへ保存する
-            </el-button>
-          </el-col>
-        </el-row>
+      <el-aside>
+        <div class="my-aside">
+          <el-row>
+            <el-col>
+              <CameraSelect v-on:connectLocalCamera="connectLocalCamera" />
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col v-if="isPossibleRecord">
+              <el-button type="primary" v-on:click="startRecording">
+                録画する
+              </el-button>
+            </el-col>
+            <el-col v-else>
+              <el-button type="info" v-on:click="stopRecording"
+                >録画を終了する</el-button
+              >
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col>
+              <el-button
+                type="primary"
+                v-if="isPossibleDownload"
+                v-on:click="downloadVideo"
+              >
+                ダウンロードする
+              </el-button>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col>
+              <el-button
+                type="success"
+                v-if="isPossibleDownload"
+                v-on:click="postVideo"
+              >
+                サーバへ保存する
+              </el-button>
+            </el-col>
+          </el-row>
+        </div>
       </el-aside>
       <el-main>
         <video
@@ -50,18 +52,46 @@
           autoplay
           playsinline
         ></video>
+        <img src="" />
       </el-main>
     </el-container>
   </el-container>
 </template>
 
 <style lang="scss">
+.my-aside {
+  border: 1px solid #ccc;
+  margin-top: 20px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+}
+.el-header {
+  background-color: #b3c0d1;
+  color: var(--el-text-color-primary);
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  text-align: center;
+}
+
+.el-main {
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
 </style>
 
 <script>
 import CameraSelect from "./CameraSelect.vue";
 import axios from "axios";
 
+// const URL = "http://3.113.248.153:8888";
+const URL = "http://localhost:82";
 export default {
   name: "Video",
   components: {
@@ -120,10 +150,26 @@ export default {
     },
     postVideo: function () {
       const blob = new Blob(this.recordedChunks, { type: "video/webm" });
-      const url =
-        "https://adrwly0e6a.execute-api.ap-northeast-1.amazonaws.com/AipaTest";
+      const data = new FormData();
+      data.append("video_data", blob);
+
+      const jobId = Math.floor(Math.random() * 100);
+      const url = URL + "/video/" + String(jobId);
+      const self = this;
       axios
-        .post(url, blob)
+        .post(url, data)
+        .then(function (response) {
+          console.log("success", response);
+          self.checkJobStatus(jobId);
+        })
+        .catch(function (error) {
+          console.log("errro", error);
+        });
+    },
+    checkJobStatus: function (jobId) {
+      const url = URL + "/video/" + String(jobId);
+      return axios
+        .get(url)
         .then(function (response) {
           console.log("success", response);
         })
