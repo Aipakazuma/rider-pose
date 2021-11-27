bdd8d5c9dabbd7a4cd1f76e3813f92f3264822a3
aipa <k.tamashiro@churadata.okinawa>
2021-11-27 11:13:44 +0900

    [modify] sassを動くようにした

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,6 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 
-createApp(App).use(ElementPlus).mount('#app')
+require('@/assets/style/global.scss');
+createApp(App).use(ElementPlus).mount('#app');
